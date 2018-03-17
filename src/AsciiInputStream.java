b07249b3202c134ca7741e05deb315b2e1fda948
1461305 <joseantonio.cegarra@e-campus.uab.cat>
2018-03-17 22:54:58 +0100

    Esto es un pull de prueba

--- src/AsciiInputStream.java
+++ src/AsciiInputStream.java
@@ -1,6 +1,7 @@
 import java.io.FilterInputStream;
 import java.io.InputStream;
 
+//Este comentario es para comprobar que el commit&push se ha hecho bien
 //Por implementar
 public class AsciiInputStream extends FilterInputStream {
 
